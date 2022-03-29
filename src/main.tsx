2d830c6f6f2f7c016efa87c8fbb6be656a092e2c
chenc <double_cl@163.com>
2022-03-29 20:02:07 +0800

    feat: change router path

--- src/main.tsx
+++ src/main.tsx
@@ -11,11 +11,11 @@ const RouterApp = () => {
       <Routes>
         <Route path='/' element={<Layout />}>
           {hasLayout.map(({ path, component: Component }) => (
-            <Route key={path} path={path} element={<Component path={path} />} />
+            <Route key={path} path={path} element={<Component />} />
           ))}
         </Route>
         {noLayout.map(({ path, component: Component }) => (
-          <Route key={path} path={path} element={<Component path={path} />} />
+          <Route key={path} path={path} element={<Component />} />
         ))}
       </Routes>
     </BrowserRouter>
